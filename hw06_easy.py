2ea099a152043b62334c4eb2d70bf77c6bde1b82
Vasiliy Trutnev <vasiliy.trutneff@yandex.ru>
2020-04-25 13:02:11 +0300

    HW06_easy upd

--- hw06_easy.py
+++ hw06_easy.py
@@ -30,43 +30,45 @@ print("Среднее геометрическое = {:.2f}".format(c))
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
-import os
-import sys
+if __name__ == '__main__':
+
+    import os
+    import sys
 
 
-def create_folder():
-    dir_name = input()
-    dir_path = os.path.join(os.getcwd(), dir_name)
-    try:
-        os.mkdir(dir_path)
-        print('директория {} создана'.format(dir_name))
-    except FileExistsError:
-        print('директория {} уже существует'.format(dir_name))
+    def create_folder():
+        dir_name = input()
+        dir_path = os.path.join(os.getcwd(), dir_name)
+        try:
+            os.mkdir(dir_path)
+            print('директория {} создана'.format(dir_name))
+        except FileExistsError:
+            print('директория {} уже существует'.format(dir_name))
 
-create_folder()
+    create_folder()
 
     
-def del_folder():
-    dir_name = input()
-    dir_path = os.path.join(os.getcwd(), dir_name)
-    try:
-        os.rmdir(dir_path)
-        print('директория {} удалена'.format(dir_name))
-    except FileNotFoundError:
-        print('директория {} не найдена'.format(dir_name))
-del_folder()
+    def del_folder():
+        dir_name = input()
+        dir_path = os.path.join(os.getcwd(), dir_name)
+        try:
+            os.rmdir(dir_path)
+            print('директория {} удалена'.format(dir_name))
+        except FileNotFoundError:
+            print('директория {} не найдена'.format(dir_name))
+    del_folder()
 # Задача-3:
 # Напишите скрипт, отображающий папки текущей директории.
-def list_dir():
-    path = 'c:/Users/Василий и Настенька/Desktop/Домашка BE'
-    print(os.listdir(path))
-list_dir()
+    def list_dir():
+        path = 'c:/Users/Василий и Настенька/Desktop/Домашка BE'
+        print(os.listdir(path))
+    list_dir()
 
 # Задача-4:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-def copy_file():
-    file_name = input()
-    import shutil
-    shutil.copy2(os.getcwd(), file_name)
-copy_file()
+    def copy_file():
+        file_name = input()
+        import shutil
+        shutil.copy2(os.getcwd(), file_name)
+    copy_file()
 
